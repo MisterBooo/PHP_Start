9a3fadc40f2d2c974f0ee848ea8dd4b77245245c
MisterBigbooo <278166530@qq.com>
2018-01-23 16:57:04 +0800

    修改action

---
+++ action.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by MisterBigbooo.
+ * User: Zeno
+ * Date: 2018/1/23
+ * Time: 上午10:48
+ */
+
+$name = $_POST['name'];
+$age = $_POST['age'];
+
+echo "name = $name , age = $age";
+echo "<hr>";
+
+
+print_r($_POST);
+
+
+
+
+?>
